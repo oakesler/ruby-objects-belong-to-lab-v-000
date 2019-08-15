be3ee4a99997a687a846252d7965b1940284a083
Olivia Kesler <oakesler@protonmail.ch>
2019-08-15 16:31:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author 
   attr_accessor :post, :name
   
-  def initialize(@post = post, @name = name)
+  def initialize(post = @post, name = @name)
     @post = post
     @name = name
   end
